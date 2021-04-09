6d9f3e49c1a54a74ea324c63249047e2a6af8532
Darrkhan <paulmarliot@gmail.com>
2021-04-09 15:51:13 +0200

    FIRST MERG ? by Darrkhan

--- Game/Darrkhan/src/app.ts
+++ Game/Darrkhan/src/app.ts
@@ -1,39 +1,67 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+//import * as BABYLON from '@babylonjs/core/Legacy/legacy';
+import { Scene } from "@babylonjs/core/scene";
+import { Engine } from '@babylonjs/core/Engines/engine';
+enum State { START = 0, GAME = 1, LOSE = 2, CUTSCENE = 3 }
 
+// App class is our entire game application
 class App {
+    // General Entire Application
+    private _scene: Scene;
+    private _canvas: HTMLCanvasElement;
+    private _engine: Engine;
+
+    /*Game State Related
+    public assets;
+    private _input: PlayerInput;
+    private _player: Player;
+    private _ui: Hud;
+    private _environment;*/
+
+    //Sounds
+    // public sfx: Sound;
+    /*public game: Sound;
+    public end: Sound;*/
+
+    //Scene - related
+    private _state: number = 0;
+    private _gamescene: Scene;
+    private _cutScene: Scene;
+
+    //post process
+    private _transition: boolean = false;
+
     constructor() {
-        // create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+        this._canvas = this._createCanvas();
+
         // initialize babylon scene and engine
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
-                } else {
-                    scene.debugLayer.show();
-                }
-            }
-        });
-        // run the main render loop
-        //nique ete mere
-        engine.runRenderLoop(() => {
-            scene.render();
-        });
+        //this._init();
     }
+    //set up the canvas
+    private _createCanvas(): HTMLCanvasElement {
+
+       //Commented out for development
+       document.documentElement.style["overflow"] = "hidden";
+       document.documentElement.style.overflow = "hidden";
+       document.documentElement.style.width = "100%";
+       document.documentElement.style.height = "100%";
+       document.documentElement.style.margin = "0";
+       document.documentElement.style.padding = "0";
+       document.body.style.overflow = "hidden";
+       document.body.style.width = "100%";
+       document.body.style.height = "100%";
+       document.body.style.margin = "0";
+       document.body.style.padding = "0";
+
+       //create the canvas html element and attach it to the webpage
+       this._canvas = document.createElement("canvas");
+       this._canvas.style.width = "100%";
+       this._canvas.style.height = "100%";
+       this._canvas.id = "gameCanvas";
+       document.body.appendChild(this._canvas);
+
+       return this._canvas;
+   }
 }
-new App();
