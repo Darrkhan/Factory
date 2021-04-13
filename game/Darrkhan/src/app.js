e7a0994fe2f50c986e7eb4aca01f1817cfd68d96
Darrkhan <paulmarliot@gmail.com>
2021-04-13 10:38:18 +0200

    .gitignore node_modules by Darrkhan

--- game/Darrkhan/src/app.js
+++ game/Darrkhan/src/app.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -21,6 +22,13 @@ function preload ()
 function create ()
 {
   this.add.image(400, 300, 'sky');
+  platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
