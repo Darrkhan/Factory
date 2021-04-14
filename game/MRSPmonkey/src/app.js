0040f12aadc45d9a5ab1b946dd9155465ff8a586
martinquivron <martinquivron@yahoo.com>
2021-04-14 15:37:23 +0200

    test

--- game/MRSPmonkey/src/app.js
+++ game/MRSPmonkey/src/app.js
@@ -22,10 +22,12 @@ var game = new Phaser.Game(config);
 
 function preload (){
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('star', 'assets/star.png');
 }
 
 function create (){
   this.add.image(400,300,'sky');
+  this.add.image(400, 300, 'star');
 }
 
 function update (){
