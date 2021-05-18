1ad54c8c434ca6c0f01bb4184e58d1ad5b0fdf4a
martinquivron <martinquivron@yahoo.com>
2021-05-18 13:52:59 +0200

    okokokokoko

--- game/MRSPmonkey/src/app.js
+++ game/MRSPmonkey/src/app.js
@@ -4,6 +4,9 @@ const config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
+    input: {
+      gamepad: true
+    },
     scene: {
         preload: preload,
         create: create,
@@ -25,7 +28,8 @@ var cursors;
 function preload (){
   this.load.image('fond', 'assets/fond.png');
   this.load.image('star', 'assets/star.png');
-  this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+  //this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+  this.load.spritesheet('dude', 'assets/perso.png', { frameWidth: 64, frameHeight: 64 });
   //this.load.spritesheet('controller-indicator', 'assets/dude1.png', 16,16);
 }
 
@@ -40,36 +44,34 @@ function create (){
   //indicator2 = this.add.sprite(10,50, 'controller-indicator');
   //indicator2.scale.x = indicator2.scale.y = 2;
   //indicator2.animations.frame = 1;
-  pad1 = this.input.gamepad.pad1;
-  pad2 = this.input.gamepad.pad2;
   this.anims.create({
       key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frames: this.anims.generateFrameNumbers('dude', { start: 4, end: 7 }),
       frameRate: 10,
       repeat: -1
   });
 
   this.anims.create({
       key: 'turn',
-      frames: [ { key: 'dude', frame: 4 } ],
+      frames: [ { key: 'dude', frame: 0 } ],
       frameRate: 20
   });
 
   this.anims.create({
       key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1
-  });
-  this.anims.create({
-      key: 'up',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frames: this.anims.generateFrameNumbers('dude', { start: 8, end: 11 }),
       frameRate: 10,
       repeat: -1
   });
   this.anims.create({
       key: 'down',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frames: this.anims.generateFrameNumbers('dude', { start: 12, end: 15 }),
+      frameRate: 10,
+      repeat: -1
+  });
+  this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
       frameRate: 10,
       repeat: -1
   });
@@ -88,34 +90,65 @@ function update (){
     //  indicator2.animations.frame = 1;
   //}
 
+  if (this.input.gamepad.total === 0)
+    {
+        return;
+    }
+
+    var pad1 = this.input.gamepad.getPad(0);
+
+    if (pad1.axes.length)
+    {
+        var axisH = pad1.axes[0].getValue();
+        var axisV = pad1.axes[1].getValue();
+        if(-1 < axisH <= 0 && -0.5 < axisV <= 0.5){
+          player1.anims.play('up',true);
+        }
+        else if(-1 < axisV <= 0 && -0.5 < axisH <= 0.5){
+          player1.anims.play('right',true);
+        }
+        else if(0 < axisH <= 1 && -0.5 < axisV <= 0.5){
+          player1.anims.play('down',true);
+        }
+        else if(0 < axisV <= 1 && -0.5 < axisH <= 0.5){
+          player1.anims.play('left', true);
+        }
+        else {
+          player1.anims.play('turn');
+        }
+        player1.x += 4 * axisH;
+        player1.y += 4 * axisV;
+
+        player1.flipX = (axisH > 0);
+    }
+  }
+
 
   if(cursors.left.isDown || cursors.right.isDown || cursors.up.isDown || cursors.down.isDown){
-    if (cursors.left.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)
+    if (cursors.left.isDown)
     {
         player1.x -= 10;
         player1.anims.play('left', true);
     }
-    if (cursors.right.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)
+    if (cursors.right.isDown)
     {
         player1.x += 10;
         player1.anims.play('right', true);
     }
-    if (cursors.up.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1)
+    if (cursors.up.isDown)
     {
         player1.y -= 10;
         player1.anims.play('up', true);
     }
-    if (cursors.down.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1)
+    if (cursors.down.isDown)
     {
         player1.y += 10;
         player1.anims.play('down', true);
     }
   }
-  else
-  {
+  else{
       player1.anims.play('turn');
   }
-}
 function render() {
     game.debug.spriteInfo(s, 20, 32);
 
